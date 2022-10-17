e6e964dccef5887554e38692b53f789efc1e02d3
Patrick Young <patrickyoung281@gmail.com>
2022-10-17 16:22:48 -0400

    commit tree d1f582c2800c7dd10276df0a17e1400e5fc795af

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+def kind
+  validate_triangle
+  if side1 == side2 && side1 == side3
+    :equilateral
+  elsif side1 == side2 || side2 == side3 || side1 == side3
+    :isosceles
+  elsif side1 != side2 && side2 != side3 && side1 !=side3 
+    :scalene
+  end
+end
+
+def verify_triangle
+    [side1, side2, side3].all?(&:positive?)
+  end
+
+def verify_inequality
+  side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2
+end
+
+def validate_triangle
+  raise TriangleError unless verify_inequality && verify_triangle
+end
+
+  class TriangleError < StandardError  
+end
+
 end
